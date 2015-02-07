b9fe0064d95f01ad5001ae23036a31da8ce744d4
Erfan <erfanimani@gmail.com>
2015-02-07 12:15:15 +0800

    Formatting...

--- app/code/community/ProxiBlue/OrderSyncQueRunner/Model/Resource/Que/Collection.php
+++ app/code/community/ProxiBlue/OrderSyncQueRunner/Model/Resource/Que/Collection.php
@@ -1,14 +1,16 @@
 <?php
 
 /**
- * 
+ *
  *
  * @category   ProxiBlue
  * @package    ProxiBlue_OrderSyncQueRunner
  * @author     Lucas van Staden (support@proxiblue.com.au)
  */
 
-class ProxiBlue_OrderSyncQueRunner_Model_Resource_Que_Collection extends Mage_Core_Model_Mysql4_Collection_Abstract {
+class ProxiBlue_OrderSyncQueRunner_Model_Resource_Que_Collection
+    extends Mage_Core_Model_Mysql4_Collection_Abstract
+{
 
     public function _construct() {
         $this->_init("ordersyncquerunner/que");
